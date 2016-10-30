1283df9149cac6f31a70b003ef621c1298b87e87
Lai Sim <laisim@netccentric.com>
2016-10-30 21:40:06 +0800

    Added in a complete list of fields for HSBC COS Result Adapter

--- src/Adapter/Result/HSBC/HSBCCOSResultAdapter.php
+++ src/Adapter/Result/HSBC/HSBCCOSResultAdapter.php
@@ -11,10 +11,16 @@ class HSBCCOSResultAdapter extends COSResultAdapterAbstract
     const OFFSET_FILE_ID = 0;
     const OFFSET_PAYMENT_ID = 1;
     const OFFSET_TRANSACTION_ID = 2;
-    const OFFSET_FULLNAME = 12;
-    const OFFSET_AMOUNT = 7;
+    const OFFSET_INSTRUMENT_TYPE = 3;
+    const OFFSET_INSTRUMENT_NUMBER = 4;
+    const OFFSET_INSTRUMENT_DATE = 5;
     const OFFSET_CURRENCY = 6;
-    const OFFSET_DATETIME = 5;
+    const OFFSET_AMOUNT = 7;
+    const OFFSET_DEBIT_DATE = 8;
+    const OFFSET_STATUS = 9;
+    const OFFSET_STATUS_DATE = 10;
+    const OFFSET_BENEFICIARY_ID = 11;
+    const OFFSET_BENEFICIARY_NAME = 12;
 
     /**
      * @param String The line to parse
@@ -26,16 +32,36 @@ class HSBCCOSResultAdapter extends COSResultAdapterAbstract
         $result -> setFileIdentifier( trim($this -> columns[self::OFFSET_FILE_ID]));
         $result -> setPaymentId( trim($this -> columns[self::OFFSET_PAYMENT_ID]));
         $result -> setTransactionId( trim($this -> columns[self::OFFSET_TRANSACTION_ID]));
-        $result -> setFullname(trim($this -> columns[self::OFFSET_FULLNAME]));
-        $result -> setAmount(trim($this -> columns[self::OFFSET_AMOUNT]));
-        $result -> setCurrency(trim($this -> columns[self::OFFSET_CURRENCY]));
+        $result -> setInstrumentType( trim($this -> columns[self::OFFSET_INSTRUMENT_TYPE]));
+        $result -> setInstrumentNumber( trim($this -> columns[self::OFFSET_INSTRUMENT_NUMBER]));
+
         //datetime is now patched to return d/m/Y
-        $datetime = \DateTime::createFromFormat('d/m/y', trim($this -> columns[self::OFFSET_DATETIME]));
+        $datetime = \DateTime::createFromFormat('d/m/y', trim($this -> columns[self::OFFSET_INSTRUMENT_DATE]));
         if( !$datetime )
-            $datetime = \DateTime::createFromFormat('d/m/Y', trim($this -> columns[self::OFFSET_DATETIME]));
-        $result -> setDateTime($datetime);
+            $datetime = \DateTime::createFromFormat('d/m/Y', trim($this -> columns[self::OFFSET_INSTRUMENT_DATE]));
+        $result -> setDateTime($datetime);        
+
+        $result -> setCurrency(trim($this -> columns[self::OFFSET_CURRENCY]));
+        $result -> setAmount(trim($this -> columns[self::OFFSET_AMOUNT]));
+
+        //debit_date is now patched to return d/m/Y
+        $debit_date = \DateTime::createFromFormat('d/m/y', trim($this -> columns[self::OFFSET_DEBIT_DATE]));
+        if( !$debit_date )
+            $debit_date = \DateTime::createFromFormat('d/m/Y', trim($this -> columns[self::OFFSET_DEBIT_DATE]));
+        $result -> setDebitDate($debit_date);  
+
+        $result -> setStatus(trim($this -> columns[self::OFFSET_STATUS]));
+
+        //status_date is now patched to return d/m/Y
+        $status_date = \DateTime::createFromFormat('d/m/y', trim($this -> columns[self::OFFSET_STATUS_DATE]));
+        if( !$status_date )
+            $status_date = \DateTime::createFromFormat('d/m/Y', trim($this -> columns[self::OFFSET_STATUS_DATE]));
+        $result -> setStatusDate($status_date);    
+
+        $result -> setBeneficiaryId(trim($this -> columns[self::OFFSET_BENEFICIARY_ID]));
+        $result -> setFullname(trim($this -> columns[self::OFFSET_BENEFICIARY_NAME]));        
+
         $this -> cosResult = $result;
     }
-
 }
 ?>
